ef541cd5752cef43a685071074691d0ca7012468
孟凡旭 <mengfanxu@qimai.cn>
2019-01-03 17:06:03 +0800

    打开html页面

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const Koa = require("koa");
 // 用于打开网站
 const opn = require("opn");
+// 用于连接数据库
 const Monk = require("monk");
-
+// 用于打开页面 
+const fs = require('fs.promised');
 
 const Router = require('koa-router');
 const router=new Router();
-const db = new Monk("localhost:27017/todos"); //链接到库
-const todos = db.get('todos'); //表
+// const db = new Monk("localhost:27017/todos"); //链接到库
+// const todos = db.get('todos'); //表
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
@@ -20,16 +22,16 @@ app.use(async (ctx, next) => {
 // 对于任何请求，app将调用该异步函数处理请求：
 router.get("/", async ctx => {
     ctx.response.type = "text/html";
-    ctx.body = "hi";
+    ctx.response.body = await fs.readFile('./pages/index.html', 'utf8');
 });
-router.get("/a", async ctx => {
-    let st = await todos.find({},function (err,docs) {
-        console.log('docs',docs);        
-    });
-    console.log('st',st);
-    ctx.response.type = "application/json";
-    ctx.body = st;
-});
+// router.get("/a", async ctx => {
+//     let st = await todos.find({},function (err,docs) {
+//         console.log('docs',docs);        
+//     });
+//     console.log('st',st);
+//     ctx.response.type = "application/json";
+//     ctx.body = st;
+// });
 // 加载路由中间件
 //解释：app.use 加载用于处理http請求的middleware（中间件），当一个请求来的时候，会依次被这些 middlewares处理。
 app.use(router.routes());
